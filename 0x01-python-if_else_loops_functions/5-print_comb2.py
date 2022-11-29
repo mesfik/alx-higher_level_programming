ead1fb616da7fea37129adb8f31ab8a80551236e
mesfik <fikreamesfin@gmail.com>
2022-11-29 18:24:45 +0000

    combo

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-import math
 for i in range(99):
-    last_digit = i % 10
-    num = math.floor(i / 10)
-    print("{:.0f}{:.0f}".format(num, last_digit), end=", ")
+    print("{:02d}".format(i), end=", ")
 print("99")

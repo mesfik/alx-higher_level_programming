b7ce09cdc7ad95b326cb08cf82c67f33688b612e
mesfik <fikreamesfin@gmail.com>
2022-12-09 21:45:15 +0000

    sort

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sorted_key = (sorted(a_dictionary.keys()))
+    for key in sorted_key:
+        print("{:s}: {}".format(key, a_dictionary[key]))

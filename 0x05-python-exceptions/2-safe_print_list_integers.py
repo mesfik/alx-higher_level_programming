70666f259669ab8c01d6181fe7e4fa50e8174a4e
vagrant <vagrant@ubuntu-18.04-amd64>
2022-12-19 18:25:34 +0000

    print

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,7 +10,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             indx += 1
         except(ValueError, TypeError):
-            break
+            continue
     print()
 
     return indx

64736b0525eba9eefdbdfcdca4fe28092c9122ba
vagrant <vagrant@ubuntu-18.04-amd64>
2022-12-19 18:00:31 +0000

    integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        if (int(value)):
-            print("{:d}".format(value))
+        print("{:d}".format(value))
         return True
     except(ValueError, TypeError):
         return False

01980dfa65c213dc473dfc8afed446a7243c31b2
mesfik <fikreamesfin@gmail.com>
2022-12-20 23:45:01 +0000

    property retrive and set

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+''' Square module to calculate area square'''
+
+
+class Square:
+
+    ''' Class square that defines a square
+
+    Args:
+        size: The value to be squared
+    '''
+
+    def __int__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        '''the size property to return size
+
+        Return: attributed private size
+        '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''the property setter size to a value
+        Args: value: the value to be setted
+              raise: valueError if not an integer
+                    TypeError if less than 0
+        '''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        '''public inatance method to return current area squared
+        Return: area aquared'''
+        return self.__size * self.__size

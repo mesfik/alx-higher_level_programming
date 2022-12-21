59aeed682b1513aae1671795914b6eb5795dbf3f
mesfik <fikreamesfin@gmail.com>
2022-12-21 22:20:56 +0000

    square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -61,5 +61,5 @@ class Square:
             for i in range(self.__position[1]):
                 print()
             for i in range(self.__size):
-                print(" " * self.__position[0], end='')
-                print('#' * self.__size)
+                print("_" * self.__position[0], end='')
+                print("#" * self.__size)

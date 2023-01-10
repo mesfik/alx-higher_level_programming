1d10f5f1ff9ae0688e440657a90baa16d12e4dde
mesfik <fikreamesfin@gmail.com>
2023-01-10 21:01:27 +0000

    write a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/bin/usr/python3
+"""
+A module to write in the text file
+"""
+
+
+def write_file(filename="", text=""):
+    """A function that writes a string to a text file (UTF8) and
+    returns the number of characters written
+    Args: filename: a name string list
+          text: a text string empty list
+    Return: the number of charactor written
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        
+        f.write(text)
+
+        return (len(text))

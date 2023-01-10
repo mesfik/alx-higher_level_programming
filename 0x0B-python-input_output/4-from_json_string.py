1cbdccf84096f5370c03e46328a475ad591a38fc
mesfik <fikreamesfin@gmail.com>
2023-01-10 21:45:23 +0000

    string load

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -9,5 +9,5 @@ def from_json_string(my_str):
     Args: my_str: string json file
     Return: an object represented by JSON string
     """
-    j_string = json.load(my_str)
+    j_string = json.loads(my_str)
     return j_string

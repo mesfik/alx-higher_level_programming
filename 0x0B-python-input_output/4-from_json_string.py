fb155f4f3801ffa70fed4b347f69f49cd2b35130
mesfik <fikreamesfin@gmail.com>
2023-01-10 21:44:12 +0000

    string load

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A module for returning an object
+"""
+import json
+
+def from_json_string(my_str):
+    """A unction that returns an object (Python data structure) represented by a JSON string:
+    Args: my_str: string json file
+    Return: an object represented by JSON string
+    """
+    j_string = json.load(my_str)
+    return j_string

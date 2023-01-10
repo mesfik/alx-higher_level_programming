ba2975c11448b18a2f4f9e275a156363cd1611d9
mesfik <fikreamesfin@gmail.com>
2023-01-10 21:33:01 +0000

    json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,7 +5,6 @@ A module used for json representation
 import json
 
 
-
 def to_json_string(my_obj):
     """A function that returns the JSON representation of an object (string)
     Args: my_obj: json string object

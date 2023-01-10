daa048c3bd280da66210a071a934eccfdbd9a9d9
mesfik <fikreamesfin@gmail.com>
2023-01-10 22:19:00 +0000

    load from file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,10 +4,11 @@ A module for creating an object
 """
 import json
 
+
 def load_from_json_file(filename):
     """A function that creates an Object from a “JSON file”:
     Args: filename: name of file string
     Return: opened file
     """
     with open(filename) as f:
-        json.load(f)
+        return json.load(f)

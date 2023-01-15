d0e5c99c890b9f90e62e4a88558f3bcbfb684ed2
mesfik <fikreamesfin@gmail.com>
2023-01-15 20:11:05 +0000

    base geometry

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,8 +6,6 @@ A module to open empty class
 
 class BaseGeometry:
     """ An empty class baseGeometry"""
-    def __init__(self):
-        return
 
     def area(self):
         """A public instance method area
@@ -17,11 +15,11 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """A public instance method that vlidates value
+        Args: name: string name
+              value: value int
         Raise: TypeError: <name> must be an integer
                ValueError: <name> must be greater than 0
         """
-        self.name = name
-        self.value = value
         if not isinstance(value, int):
             raise TypeError("<name> must be an integer")
         if value <= 0:

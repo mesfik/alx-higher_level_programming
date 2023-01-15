069369f42f19cdc1ef62324a85df167a500ee5cd
mesfik <fikreamesfin@gmail.com>
2023-01-15 23:04:39 +0000

    add

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A module to add attribute
+"""
+
+
+def add_attribute(obj, attr, val):
+    """A function to add an attribute if possible
+    Args: obj: object attribute
+          attr: attribute to be checked
+          val: value of object attribute
+    Raise: TypeError: can't add new attribute
+    """
+    if not hasattr(obj, '__class__'):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, attr, val)

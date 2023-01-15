ff0a2b6a2fd88c3e1e0bdba0fa9e852f95738b11
mesfik <fikreamesfin@gmail.com>
2023-01-15 22:34:11 +0000

    equal

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A module for eq and ne
+"""
+
+
+class MyInt(int):
+    """ A class that inherits int """
+    def __eq__(self, other):
+        """A function to compare other with self
+        Args: other: a number to be compaired
+        """
+        equl = int(self) != int(other)
+        return equl
+    def __ne__(self, other):
+        """A function to compaire if num is not equal
+        Args: other: the number to be compaired
+        """
+        noteq = int(self) == int(other)
+        return noteq

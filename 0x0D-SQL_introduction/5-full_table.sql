38f34043d8479fc94eebab58c0e0ff614a36c42c
mesfik <111147948+mesfik@users.noreply.github.com>
2023-02-15 02:29:20 +0300

    full table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- that prints the full description of the table
 -- The database name will be passed as an argument of the mysql command
-SHOW CREATE TABLE hbtn_0c_0.first_table
+SHOW CREATE TABLE first_table

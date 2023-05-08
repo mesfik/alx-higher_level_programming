6335f2a4daf8aea3f506103a4da229e0dbf96376
mesfik <fikreamesfin@gmail.com>
2023-05-08 14:11:28 +0000

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+
+from models.base import Base
+
+"""
+Rectangle module
+"""
+
+
+class Rectangle(Base):
+    """
+    A clss Rectangle that inheits from base class
+    Args: width: private instancce of the width of the rectangle with
+        getter and setter
+         height: private instance of the height of the rectangle with
+        getter and setter
+          x: private instance x with getter and setter
+          y: private instance y with getter and setter
+          id: public instance attribute id
+          """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ instantiating function"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+        @property
+        def width(self):
+            """private instance function
+            Return: private of width
+            """
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """private instance function to be setted
+            Args: value: value to be  setted
+            """
+            self.__width = value
+
+        @property
+        def height(self):
+            """private instance function
+            Return: private of height
+            """
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            """private instance function to be setted
+            Args: value: value to be  setted
+            """
+            self.__height = value
+
+        @property
+        def x(self):
+            """private instance function
+            Return: private of x
+            """
+            return self.__x
+
+        @x.setter
+        def x(self, value):
+            """
+            private instance function to be setted
+            Args: value: value to be setted in x
+            """
+            self.__x = value
+
+        @property
+        def y(self):
+            """
+            private instance function
+            Return: private of y
+            """
+            return self.__y
+
+        @y.setter
+        def y(self, value):
+            """
+            private instance function to be setted
+            Args: value: value to be setted in y
+            """
+            self.__y = value

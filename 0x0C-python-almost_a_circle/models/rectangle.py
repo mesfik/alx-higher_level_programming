784adcf54853acc2e3910145810b24d9804f4b1b
mesfik <111147948+mesfik@users.noreply.github.com>
2023-05-13 14:33:36 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,10 +18,10 @@ class Rectangle(Base):
           """
     def __init__(self, width, height, x=0, y=0, id=None):
         """ instantiating function"""
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
         @property

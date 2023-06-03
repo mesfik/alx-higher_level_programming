1ce7bfe962cde7eb1ef8bebf26af076c444f4ee9
mesfik <fikreamesfin@gmail.com>
2023-06-04 02:32:34 +0300

    greater than 10

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
 -- script that lists all records with a score >= 10
--- scores ordered by score (top first)
+-- Records are ordered by descending score.
 SELECT `name`, `score`
 FROM `second_table`
 WHERE `score` >= 10

13ea46ffe212ed4291b60d7e8db7a189bfe2260f
mesfik <fikreamesfin@gmail.com>
2023-06-08 23:04:08 +0300

    grant privilage

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- a script that lists all privileges of the MySQL users
 -- privilage is added for user_0d_1 and user_0d_2
-SHOW GRANT FOR 'user_0d_1'@'localhost';
-SHOW GRANT FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

c5d39a0a85c51fb84fb84b64187a1e8b15928a5e
mesfik <fikreamesfin@gmail.com>
2023-06-09 22:59:59 +0300

    joining states

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- a script that lists all cities contained in the database hbtn_0d_usa.
+-- Results are sorted in ascending order by cities.id
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;

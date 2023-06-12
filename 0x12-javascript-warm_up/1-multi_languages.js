a11daba0c82506af331479dcea49c1937a43ce5d
mesfik <fikreamesfin@gmail.com>
2023-06-12 23:03:29 +0300

    arguments

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
 
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+
+let x = 1;
+for (; x <= 3; x++){
+	if (x === 1){
+		console.log("C is fun")
+	} else if (x === 2){
+		console.log("Python is cool")
+	} else{
+		console.log("JavaScript is amazing")
+	}
+}
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+let firstArg = 0;
+
+for (let i = 2; process.argv[i]; i++){
+	firstArg++;
+	if (firstArg === 1){
+		console.log(process.argv[i])
+	}
+}
+if (firstArg === 0){
+	console.log("No argument")
+}

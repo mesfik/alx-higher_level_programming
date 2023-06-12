ef39bec18f00b7b2ea2a7346e8b099f44b9af805
mesfik <fikreamesfin@gmail.com>
2023-06-12 23:17:52 +0300

    semistandard

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,12 +2,12 @@
 
 let firstArg = 0;
 
-for (let i = 2; process.argv[i]; i++){
-	firstArg++;
-	if (firstArg === 1){
-		console.log(process.argv[i])
-	}
+for (let i = 2; process.argv[i]; i++) {
+  firstArg++;
+  if (firstArg === 1) {
+    console.log(process.argv[i]);
+  }
 }
-if (firstArg === 0){
-	console.log("No argument")
+if (firstArg === 0) {
+  console.log('No argument');
 }

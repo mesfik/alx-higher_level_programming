5ee37b8fe11f86ebe35e025779027e6522ec55cb
mesfik <fikreamesfin@gmail.com>
2023-06-12 23:14:50 +0300

    semistand

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-
 let x = 1;
-for (; x <= 3; x++){
-	if (x === 1){
-		console.log("C is fun")
-	} else if (x === 2){
-		console.log("Python is cool")
-	} else{
-		console.log("JavaScript is amazing")
-	}
+for (; x <= 3; x++) {
+  if (x === 1) {
+    console.log('C is fun');
+  } else if (x === 2) {
+    console.log('Python is cool');
+  } else {
+    console.log('JavaScript is amazing');
+  }
 }

4942e20e2ee23bbd915f631de302b54e79415e5d
mesfik <fikreamesfin@gmail.com>
2023-06-19 00:19:13 +0300

    filter cities by name

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Lists all cities with a name starting with 
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    city_name = sys.argv[4]
+    query = """
+            SELECT * 
+            FROM cities
+            INNER JOIN states ON cities.state_id = states.id
+            WHERE cities.name LIKE %s
+            ORDER BY cities.id ASC;
+            """
+    cur.execute(query, (city_name + "%",))
+    cities = cur.fetchall()
+
+    for city in cities:
+        print(city)

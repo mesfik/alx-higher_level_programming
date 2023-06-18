325ed260bb75ce0150c799313d140f89379a5649
mesfik <fikreamesfin@gmail.com>
2023-06-19 02:10:10 +0300

    model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
 
+"""
+The model state module
+
+define the first stae model
+a python file that contains the class definition of a
+State and an instance Base = declarative_base():
+
+"""
+
+
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
 from sqlalchemy import String, Integer, Column
 import sys
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
 
-"""
-python file that contains the class definition
-of a State and an instance
 Base = declarative_base()
-"""
 
 
 class State(Base):
+    """"
+    inherits from Base Tips
+    id: that represents a column of an auto-generated,    unique integer,
+    can’t be null and is a primary key
+    name: that represents a column of a string with maximum 128 characters
+    and can’t be null
     """
-    a state class that inherited from base
-    """
+
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, unique=True, autoincrement=True,
-            nullable=False)
+    id = Column(Integer, autoincrement=True, unique=True, nullable=True,
+                primary_key=True)
     name = Column(String(128), nullable=False)

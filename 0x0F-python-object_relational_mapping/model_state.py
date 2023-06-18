7e9ac783e778141744760995046b6ce23e5033f8
mesfik <fikreamesfin@gmail.com>
2023-06-19 01:48:59 +0300

    model state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+"""
+python file that contains the class definition
+of a State and an instance
+Base = declarative_base()
+"""
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    a state class that inherited from base
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)

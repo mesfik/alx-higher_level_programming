03b610ea7d09160f5d30a69dd72bd788d0880217
mesfik <fikreamesfin@gmail.com>
2023-06-19 18:33:59 +0300

    state insert

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+module to add a new name states databases
+"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+
+def add_new():
+    """
+    function to add new name states
+    """
+    user = "mysql+mysqldb://{}:{}@localhost:3306/{}"\
+        .format(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(user, echo=False)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+
+if __name__ == "__main__":
+    add_new()

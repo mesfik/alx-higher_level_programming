b72ec91763fcfbd54af4053b3c22232566f9f07c
mesfik <fikreamesfin@gmail.com>
2023-06-19 21:57:43 +0300

    cities all

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -35,4 +35,4 @@ class City(Base):
     state = relationship("State", back_populates="cities")
 
 
-# State.cities = relationship("City", order_by=City.id, back_populates="state")
+State.cities = relationship("City", order_by=City.id, back_populates="state")

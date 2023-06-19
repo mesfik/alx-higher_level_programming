11ffd507885e327d055065fa51c60c18fd8eb786
mesfik <fikreamesfin@gmail.com>
2023-06-19 14:30:01 +0300

    state by name

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -16,17 +16,17 @@ def list_having_name_from_states():
     user = "mysql+mysqldb://{}:{}@localhost:3306/{}"\
         .format(sys.argv[1], sys.argv[2], sys.argv[3])
 
-    name = sys.argv[4]
+    nama = sys.argv[4]
     engine = create_engine(user, echo=False)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.name == name)\
-        .order_by(State.id).first():
-            if state is not None:
-                print(state[0])
-            else:
-                print("Not found")
+    st = session.query(State).filter(State.name == nama)\
+        .order_by(State.id).first()
+    if st is not None:
+        print(st[0])
+    else:
+        print("Not found")
 
 
 if __name__ == "__main__":

c07b007e463da0ed5fcd3b7f4df388bbf507e9cb
mesfik <fikreamesfin@gmail.com>
2023-06-19 21:31:28 +0300

    cities all

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+query all the cities
+"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_city import City
+import sys
+
+
+def list_cities():
+    """
+    list all cities
+    """
+    user = "mysql+mysqldb://{}:{}@localhost:3306/{}"\
+        .format(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(user, echo=False)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state, city in session.query(State, City).\
+        join(State, City.state_id == State.id).order_by(State.id):
+            print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+
+if __name__ == "__main__":
+    list_cities()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""
+The model city module
+
+define the first city model
+a python file that contains the class definition of a
+State and an instance Base = declarative_base():
+
+"""
+
+
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import String, Integer, Column, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
+import sys
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """"
+    inherits from Base Tips
+    id: that represents a column of an auto-generated,unique integer,
+    can’t be null and is a primary key
+    name: that represents a column of a string with maximum 128 characters
+    and can’t be null
+    """
+
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, unique=True, nullable=False,
+                primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    state = relationship("State")
+
+    def __init__(self, name, state_id):
+        """
+        instantiation function 
+        """
+        self.name = name
+        self.state_id = state_id

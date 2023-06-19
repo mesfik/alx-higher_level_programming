2d08f94c677cc6d34f18a30891e04970ef7d2bb7
mesfik <fikreamesfin@gmail.com>
2023-06-19 14:12:35 +0300

    state by name

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+query 'name' containing from states databases
+"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+
+def list_having_name_from_states():
+    """
+    list states with name passed
+    """
+    user = "mysql+mysqldb://{}:{}@localhost:3306/{}"\
+        .format(sys.argv[1], sys.argv[2], sys.argv[3])
+
+    name = sys.argv[4]
+    engine = create_engine(user, echo=False)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id).all():
+        if name in state.name:
+            print("{}".format(state.id))
+
+
+if __name__ == "__main__":
+    list_having_name_from_states()

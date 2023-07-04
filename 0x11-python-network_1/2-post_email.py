ea4be2e0de42a0f6db0c635dd8c3a74163045f44
mesfik <fikreamesfin@gmail.com>
2023-07-05 01:35:14 +0300

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-
 """
-Python script that takes in a URL and an email,
-sends a POST request to the passed URL with the email as a parameter,
-and displays the body of the response (decoded in utf-8)
+2-post_email module
+Post an email - urllib headers
 """
 
 
-from urllib.request import urlopen
-from urllib.parse import urlencode
-import urllib.request
+from urllib import request, parse
 import sys
+
 
 if __name__ == "__main__":
 
     url = sys.argv[1]
-    email = sys.argv[2]
-    data = {"email": email}
+    Email = sys.argv[2]
 
-    data = urlencode(data)
+    value = {'email': Email}
+
+    data = parse.urlencode(value)
     data = data.encode('ascii')
 
-    req = urllib.request.Request(url, data)
-
-    with urlopen(req) as response:
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
         html = response.read()
 
-    html = html.encode("utf-8")
+    html = html.decode('utf-8')
     print(html)

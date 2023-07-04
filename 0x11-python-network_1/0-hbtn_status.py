4cb247397dce42ed156e5eedabb24d0b3bca6a9d
mesfik <fikreamesfin@gmail.com>
2023-07-05 00:11:34 +0300

    urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,12 +6,12 @@ python3 for url request
 
 from urllib.request import urlopen
 
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urlopen(url) as response:
+        data = response.read()
 
-url = "https://alx-intranet.hbtn.io/status"
-with urlopen(url) as response:
-    data = response.read()
-
-print("Body response:")
-print("\t- type: {}".format(type(data)))
-print("\t- content: {}".format(data))
-print("\t- utf8 content: {}".format(data))
+    print("Body response:")
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
+    print("\t- utf8 content: {}".format(data))

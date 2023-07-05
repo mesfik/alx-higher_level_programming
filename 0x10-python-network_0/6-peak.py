b28b5b5c5a1a4f1263664388cfb352f2c3ff8bf5
mesfik <fikreamesfin@gmail.com>
2023-07-06 00:12:04 +0300

    find peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+finding the peak number
+"""
+
+def find_peak(list_of_integers):
+    """
+    find the peak number from a list
+    Args: list_of_integers: list of numbers
+    """
+    if list_of_integers:
+        list_of_integers.sort()
+        return (list_of_integers[-1])
+
+if __name__ == "__main__":
+    find_peak()
+

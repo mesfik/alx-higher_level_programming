352eca0cae53645db0bc51c8d6ed3217e1bb5899
mesfik <fikreamesfin@gmail.com>
2023-07-05 18:58:30 +0300

    grep

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-curl -Is "$1" | grep Content-Length | cut -d ' ' -f 2
+curl -sI "$1" | grep Content-Length | cut -d ' ' -f 2

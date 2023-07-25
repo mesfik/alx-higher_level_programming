2a3f4637a9313338ad4bffce0b5d621c249e79d1
mesfik <fikreamesfin@gmail.com>
2023-07-25 23:32:45 +0300

    completed task in todo

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,30 +1,21 @@
 #!/usr/bin/node
-
 const request = require('request');
-const process = require('process');
 
-const url = process.argv[2];
-
-request(url, (error, response, body) => {
+// The first argument is the API URL
+const baseURL = process.argv[2];
+request(baseURL, (error, response, body) => {
+  const aggregate = {};
   if (error) {
     console.log(error);
-  } else if (response.statusCode !== 200) {
-    console.log('error:', response.statusCode);
-  } else {
-    try {
-      const taskCompleted = {};
-      const tasks = JSON.parse(body);
-      for (const task of tasks) {
-        if (task.completed) {
-          if (!taskCompleted[task.userId]) {
-            taskCompleted[task.userId] = 0;
-          }
-          taskCompleted[task.userId]++;
-        }
-      }
-      console.log(taskCompleted);
-    } catch (error) {
-      console.log(error);
-    }
   }
+  const json = JSON.parse(body);
+  json.forEach(element => {
+    if (element.completed) {
+      if (!aggregate[element.userId]) {
+        aggregate[element.userId] = 0;
+      }
+      aggregate[element.userId]++;
+    }
+  });
+  console.log(aggregate);
 });

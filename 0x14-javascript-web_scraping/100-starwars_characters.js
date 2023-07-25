ceb7624f2df78e851442e7ede0d38b0b90e6ae81
mesfik <fikreamesfin@gmail.com>
2023-07-26 00:05:14 +0300

    star war characters

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // prints all characters of a Star Wars movie
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/' + process.argv[2];
 
 request(url, function (error, response, body) {
   if (error) {

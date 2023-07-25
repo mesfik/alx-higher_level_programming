f31fdf2594bcdf16528d4e6a816648fe9df738bc
mesfik <fikreamesfin@gmail.com>
2023-07-25 17:52:40 +0300

    readfile

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+
+const process = require('process');
+const fs = require('fs');
+
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', (error, content) => {
+	if (error) {
+		console.log(error)
+	}
+	else {
+		console.log(content)
+	}
+});
